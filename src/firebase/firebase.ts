5da4be61b3e67b4d1b2201df09bc76564e988add
ricardolsousa <ricardolsousa2909@gmail.com>
2025-01-12 22:56:32 +0000

    feat: firebase project config

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID",
+  apiKey: "AIzaSyCQJdrmS7BP-libheLK-BPvSDh0Z3-SKqw",
+  authDomain: "rent-spaces-e5f5d.firebaseapp.com",
+  projectId: "rent-spaces-e5f5d",
+  storageBucket: "rent-spaces-e5f5d.firebasestorage.app",
+  messagingSenderId: "626421544376",
+  appId: "1:626421544376:web:0999279961592f146a0b4d",
 };
 
 // Initialize Firebase
