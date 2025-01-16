d905ce3b09793b794ed7177fd488ed1e4cd6063e
ricardolsousa <ricardolsousa2909@gmail.com>
2025-01-16 01:04:18 +0000

    feat: added validation to just create space when user type is owner

--- src/components/spaces/space-list/space-list-actions/SpaceListActions.tsx
+++ src/components/spaces/space-list/space-list-actions/SpaceListActions.tsx
@@ -1,9 +1,15 @@
+import { useSelector } from "react-redux";
 import CreateSpace from "../../create-space/CreateSpace";
+import { AuthenticationStateProps } from "../../../../types/authentication/AuthenticationTypes";
 
 const SpaceListActions = () => {
+  const userDetails = useSelector(
+    (state: AuthenticationStateProps) => state.auth.userDetails
+  );
+
   return (
     <div className="w-full flex justify-end mb-4 gap-2">
-      <CreateSpace />
+      {userDetails?.userType === "owner" && <CreateSpace />}
     </div>
   );
 };
