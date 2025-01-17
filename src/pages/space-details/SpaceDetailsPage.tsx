a70a99a7c103cee07ca062e04941b43fa8e6a812
ricardolsousa <ricardolsousa2909@gmail.com>
2025-01-17 19:29:26 +0000

    feat: get space by id and show it on space details page

--- src/pages/space-details/SpaceDetailsPage.tsx
+++ src/pages/space-details/SpaceDetailsPage.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getSpace } from "../../services/space/SpaceService";
+
 const SpaceDetailsPage = () => {
-  return <div className="py-4 px-48">Space Details Page</div>;
+  const { spaceId } = useParams();
+  const [space, setSpace] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchSpace = async () => {
+      try {
+        if (spaceId) {
+          const newSpace = await getSpace(spaceId);
+
+          if (newSpace) {
+            setSpace(newSpace);
+          }
+        }
+      } catch (e) {
+        console.error(e);
+        throw new Error("Error trying to get space");
+      }
+    };
+
+    fetchSpace();
+  }, [spaceId]);
+
+  return (
+    <div className="py-4 px-48">
+      Space Details Page {spaceId}
+      <div>{JSON.stringify(space, null, 2)}</div>
+    </div>
+  );
 };
 
 export default SpaceDetailsPage;
