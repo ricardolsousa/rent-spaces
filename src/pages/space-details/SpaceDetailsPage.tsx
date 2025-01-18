6f45586a71bf91b3eb1af2de883699484bc4afc0
ricardolsousa <ricardolsousa2909@gmail.com>
2025-01-18 01:44:13 +0000

    feat: added details and grids for images to space details page

--- src/pages/space-details/SpaceDetailsPage.tsx
+++ src/pages/space-details/SpaceDetailsPage.tsx
@@ -27,8 +27,29 @@ const SpaceDetailsPage = () => {
 
   return (
     <div className="py-4 px-48">
-      Space Details Page {spaceId}
-      <div>{JSON.stringify(space, null, 2)}</div>
+      {space && (
+        <div>
+          <h2 className="text-2xl font-bold text-neutral-950">{space.title}</h2>
+          <div className="flex items-center gap-2">{space.address}</div>
+          <div className="flex flex-col gap-1">
+            <div className="grid grid-cols-2 gap-1 grid-cols-5">
+              <div className="w-full bg-black col-span-3"></div>
+              <div className="grid grid-rows-2 gap-1 col-span-2">
+                <div className="w-full h-3 bg-black"></div>
+                <div className="w-full h-3 bg-black"></div>
+              </div>
+            </div>
+            <div className="grid grid-cols-5 gap-1">
+              <div className="h-3 bg-black"></div>
+              <div className="h-3 bg-black"></div>
+              <div className="h-3 bg-black"></div>
+              <div className="h-3 bg-black"></div>
+              <div className="h-3 bg-black"></div>
+            </div>
+          </div>
+          <div>{JSON.stringify(space, null, 2)}</div>
+        </div>
+      )}
     </div>
   );
 };
