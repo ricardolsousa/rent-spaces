50dba6cbbb32dd0e320847ea68a86b35c30fa987
ricardolsousa <ricardolsousa2909@gmail.com>
2025-01-21 17:50:29 +0000

    feat: getReservationsBySpaceId function added

--- src/services/reservation/ReservationService.ts
+++ src/services/reservation/ReservationService.ts
@@ -1,4 +1,11 @@
-import { addDoc, collection, getDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "../../firebase/firebase";
 
 export const createReservation = async (reservation: any) => {
@@ -24,5 +31,29 @@ export const createReservation = async (reservation: any) => {
   } catch (e) {
     console.error(e);
     throw new Error("Error trying to create reservation");
+  }
+};
+
+export const getReservationsBySpaceId = async (spaceId: string) => {
+  try {
+    const reservationsRef = collection(db, "reservations");
+
+    const reservationQuery = query(
+      reservationsRef,
+      where("spaceId", "==", spaceId),
+      where("status", "==", "confirmed")
+    );
+
+    const reservationsSnapshot = await getDocs(reservationQuery);
+
+    const reservations = reservationsSnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+
+    return reservations;
+  } catch (e) {
+    console.error(e);
+    throw new Error("Error trying to get reservations for the space");
   }
 };
